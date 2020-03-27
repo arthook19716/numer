81b890dc7ad166147319e6d9bb42ca50357e4a3c
arthook19716 <62649329+arthook19716@users.noreply.github.com>
2020-03-27 16:38:39 +0700

    Add files via upload

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 let Trap = require('../models/Trap');
 let Bisection = require('../models/Bisection');
+let FalsePosition = require('../models/FalsePosition');
+let Onepoint = require('../models/Onepoint');
+let Newton = require('../models/Newton');
 
 /* GET users listing. */
 
@@ -27,7 +30,7 @@ router.post('/addtrap',(req,res)=>{
   })
 })
 
-
+/*----------------- Bisection-------------------------*/
 router.get('/showBisection', function(req, res, next) {
  
   Bisection.find().sort({age:1}).exec((err,data)=>{
@@ -41,6 +44,68 @@ router.get('/showBisection', function(req, res, next) {
 router.post('/addBisection',(req,res)=>{
   console.log(req.body);
   let doc = new Bisection(req.body);
+  doc.save((err,data)=>{
+    if(err) throw err;
+    res.send({success:true});
+  })
+})
+
+/*-------------------------False-Position----------------------*/
+
+router.get('/showFalsePosition', function(req, res, next) {
+ 
+   FalsePosition.find().sort({age:1}).exec((err,data)=>{
+    console.log(data);
+    return res.json({success:true,data:data});
+  })
+
+});
+
+
+router.post('/addFalsePosition',(req,res)=>{
+  console.log(req.body);
+  let doc = new FalsePosition(req.body);
+  doc.save((err,data)=>{
+    if(err) throw err;
+    res.send({success:true});
+  })
+})
+
+
+/*------------------One-point----------------------------*/
+router.get('/showOnepoint', function(req, res, next) {
+ 
+  Onepoint.find().sort({age:1}).exec((err,data)=>{
+    console.log(data);
+    return res.json({success:true,data:data});
+  })
+
+});
+
+
+router.post('/addOnepoint',(req,res)=>{
+  console.log(req.body);
+  let doc = new Onepoint(req.body);
+  doc.save((err,data)=>{
+    if(err) throw err;
+    res.send({success:true});
+  })
+})
+
+/*--------------------Newton-------------------------*/
+router.get('/showNewton', function(req, res, next) {
+ 
+  Newton.find().sort({age:1}).exec((err,data)=>{
+    console.log(data);
+    return res.json({success:true,data:data});
+  })
+
+});
+
+
+router.post('/addNewton',(req,res)=>{
+  console.log(req.body);
+  let doc = new Newton(req.body);
   doc.save((err,data)=>{
     if(err) throw err;
     res.send({success:true});
